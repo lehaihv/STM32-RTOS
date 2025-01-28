49e9d739a0515ff0c998bd90290ee7d96e533c6b
Hai LE <haihv@yahoo.com>
2025-01-28 07:41:03 -0500

    Dell 7740

---
+++ UF2024/Nucleo_H755ZI/New Folder/App_1/CM7/tmp/App_1_CM7-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "D:/GitHub/STM32-RTOS/UF2024/Nucleo_H755ZI/New Folder/App_1/CM7"
+  "D:/GitHub/STM32-RTOS/UF2024/Nucleo_H755ZI/New Folder/App_1/CM7/build"
+  "D:/GitHub/STM32-RTOS/UF2024/Nucleo_H755ZI/New Folder/App_1/CM7"
+  "D:/GitHub/STM32-RTOS/UF2024/Nucleo_H755ZI/New Folder/App_1/CM7/tmp"
+  "D:/GitHub/STM32-RTOS/UF2024/Nucleo_H755ZI/New Folder/App_1/CM7/src/App_1_CM7-stamp"
+  "D:/GitHub/STM32-RTOS/UF2024/Nucleo_H755ZI/New Folder/App_1/CM7/src"
+  "D:/GitHub/STM32-RTOS/UF2024/Nucleo_H755ZI/New Folder/App_1/CM7/src/App_1_CM7-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/GitHub/STM32-RTOS/UF2024/Nucleo_H755ZI/New Folder/App_1/CM7/src/App_1_CM7-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/GitHub/STM32-RTOS/UF2024/Nucleo_H755ZI/New Folder/App_1/CM7/src/App_1_CM7-stamp${cfgdir}") # cfgdir has leading slash
+endif()
